67787a9904170e08c776b7b9012ebd566571894e
JHebel1 <jakubhebelll@gmail.com>
2026-04-12 23:30:31 +0200

    feat: tests num: 5, 6, 8, 10, 12, 13

--- BackendA/BackendA/Program.cs
+++ BackendA/BackendA/Program.cs
@@ -20,5 +20,38 @@ app.MapGet("/api/slow", async () =>
     return Results.Ok("slow-A");
 });
 app.MapGet("/api/large", () => Results.Text(new string('X', 100000)));
+app.MapPost("/api/large", async context =>
+{
+    var buffer = new byte[1024 * 8];
+    long totalRead = 0;
+    int read;
+    while ((read = await context.Request.Body.ReadAsync(buffer, 0, buffer.Length)) > 0)
+    {
+        totalRead += read;
+    }
 
+    await context.Response.WriteAsync(totalRead.ToString());
+});
+app.MapGet("/api/cookies", async context =>
+{
+    if (context.Request.Cookies.TryGetValue("test-cookie", out var cookies))
+    {
+        await context.Response.WriteAsync(cookies);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("cookies not found");
+    }
+});
+app.MapGet("/api/test-headers", async context =>
+{
+    var headers = new Dictionary<string, string>
+    {
+        { "X-Forwarded-For", context.Request.Headers["X-Forwarded-For"].ToString() },
+        { "X-Forwarded-Proto", context.Request.Headers["X-Forwarded-Proto"].ToString() },
+        { "X-Forwarded-Host", context.Request.Headers["X-Forwarded-Host"].ToString() }
+    };
+    await context.Response.WriteAsJsonAsync(headers);
+});
 app.Run("http://0.0.0.0:8080");
